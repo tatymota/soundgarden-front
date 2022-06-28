f4dfbf2ee75fb85abc9483d73280989476f94191
Pedro Fontes <pedro7fontes@gmail.com>
2022-06-27 21:46:48 -0300

    Create cadastro-evento.js
    Adiciona função de cadastro de evento

---
+++ script/cadastro-evento.js
@@ -0,0 +1,31 @@
+let botaoCriarEvento = document.getElementById("botao-criar-evento"); // acesso ao botão "criar evento" pelo ID
+let formulario = document.getElementById("formulario-cadastro-evento"); // acesso ao formulário pelo ID
+
+ function cadastrarEvento() {
+     const body = { //JSON com as informações necessárias para criar o evento
+         name: formulario[0].value,
+         poster: formulario[1].value,
+         attractions: formulario[2].value.split(","), // utilizado o split para separar a string com "," e transformando em array
+         description: formulario[3].value,
+         scheduled: formulario[4].value, //conversão do formato da data para ISO
+         number_tickets:formulario[5].value,
+     }
+  
+    fetch("https://xp41-soundgarden-api.herokuapp.com/events", {  // requisição para o servidor
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "body": JSON.stringify(body)
+  })
+    .then(response => {
+      console.log(response);
+      alert("Evento cadastrado com sucesso");
+      window.location.replace("./admin.html");
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+ botaoCriarEvento.addEventListener("click", cadastrarEvento, false); //adicionando callback ao evento de click 
